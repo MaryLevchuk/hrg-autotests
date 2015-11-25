3aa7f86d7b7a582236ec6e885f7e2345f5a71a00
unknown <maria.levchuk@pc-maryl.co.int>
2015-11-25 15:44:01 +0200

    step1

---
+++ HRGAutoTests/Helpers/Steps/step1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+//using Settings;
+using Helpers;
+
+namespace Helpers.Steps
+{ 
+    public class step1
+    {
+        public step1()
+        {
+            IWebElement menuPanel;
+            IWebElement logo;
+            List<IWebElement> primaryItems;
+            List<IWebElement> secondaryItems;
+            IWebElement languageSelector;
+
+            IWebElement footerPanel;
+            List<IWebElement> socialIcons;
+            List<IWebElement> infoItems;
+            IWebElement copyright;
+            List<IWebElement> collaborativeLogos;
+            IWebElement arrowUp;
+
+            IWebElement header;
+            IWebElement intro;
+            IWebElement bookingVoyagePanel;
+            IWebElement findAdventure;
+            List<IWebElement> promotedTravels;
+
+        } 
+    }
+}
